4174db5cff81b9debf2b8f1faf872e91fbaf145a
Rajajarfani <rajajarfani@gmail.com>
2024-02-16 13:14:58 +0100

    model info

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module oncf2 {
+	requires java.desktop;
+	requires jcalendar;
+	requires java.sql;
+}
--- src/voyageurs/ReservationsInterface.java
+++ src/voyageurs/ReservationsInterface.java
@@ -1,6 +1,6 @@
 package voyageurs;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
